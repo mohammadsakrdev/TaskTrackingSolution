043a6d8d96d28acfc5fd9d42c4efea62dd0bb078
mohammad sakr <mohammad_sakr_98@yahoo.com>
2017-06-27 05:31:44 +0200

    updates

--- TaskTracking/Controllers/EmployeesController.js
+++ TaskTracking/Controllers/EmployeesController.js
@@ -1,18 +1,21 @@
 ﻿(function () {
-    var EmployeesController = function ($scope, $http) {
-        var employees = function (serviceResp) {
-            $scope.Employees = serviceResp.data;
+    var EmployeesController = function ($scope, employeeService, $log) {
+        var employees = function (data) {
+            $scope.Employees = data;
         };
-        $scope.SearchEmployees = function (EmployeeName) {
-            $http.get("http://localhost:57786/api/employees/" + EmployeeName)
+        $scope.SearchEmployees = function (employeeName) {
+            employeeService.SearchEmployees(employeeName)
             .then(employees, errorDetails);
+            $log.info('Found Employee which contains - ' + employeeName);
         };
         var errorDetails = function (serviceResp) {
             $scope.Error = "Something went wrong ??";
         };
-        $http.get("http://localhost:57786/api/employees")
-            .then(employees, errorDetails);
+        employeeService.employees().then(employees, errorDetails);
         $scope.Title = "Employee Details Page";
+        $scope.EmployeeName = null;
     };
-    app.controller("EmployeesController", EmployeesController);
+
+    app.controller("EmployeesController", ["$scope", "employeeService", "$log", EmployeesController]);
+
 }());
---
+++ TaskTracking/Services/EmployeesService.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var employeeService = function ($http) {
+        var employees = function () {
+            return $http.get("http://localhost:2464/api/employees")
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+        var searchEmployees = function (employeeName) {
+            return $http.get("http://localhost:2464/api/employees/" + employeeName)
+            .then(function (serviceResp) {
+                return serviceResp.data;
+            });
+        };
+        return {
+            employees: employees,
+            searchEmployees: searchEmployees
+        };
+    };
+    var module = angular.module("TaskTrackingModule");
+    module.factory("employeeService", ['$http', employeeService]);
+}());
